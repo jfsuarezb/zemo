149db3d6dc48cd95e73b4add3fc8bec6f134aa5a
Estudio <jfsuarezb@icloud.com>
2020-09-12 22:55:10 -0500

    Implemented add user handler which, after an add_user subcommand, encrypts the appropiate data and saves it in files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log(require("./parser")())
+let add_user_handler = require("./add_user.js")
+
+let parseObject = require("./parser.js")()
+
+if (parseObject.sctype == "add_user") {
+
+	add_user_handler(parseObject.username, parseObject.password)
+	console.log(`Saved encrypted credentials at ${process.env.HOME}/.zemo.config`)
+	process.exit()
+
+} 
